7bf61b43a6f79a815d6a8057a764f08a59f48fa9
Davide Guerri <davide.guerri@gmail.com>
2015-09-01 22:38:24 +0100

    Add nova key delete

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -17,6 +17,7 @@
 #
 
 neutron net-delete external
+nova keypair-list | awk '/([0-9a-f]{2}\:){15}/ {print $2}' | xargs nova keypair-delete
 nova flavor-list | awk '/ParallelsVM/ { print $2}' | xargs nova flavor-delete
 glance image-list | awk '/\| [0-9a-f]/ { print $2}' | xargs glance image-delete
 ironic node-list | awk '/\| [0-9a-f]/ { print $2}' | xargs ironic node-delete
