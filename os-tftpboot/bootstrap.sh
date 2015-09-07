2cc9e76bf148ad5ff1fde05853df70d773652d85
Davide Guerri <davide.guerri@gmail.com>
2015-09-07 11:26:26 +0100

    Fix tftp map file

--- os-tftpboot/bootstrap.sh
+++ os-tftpboot/bootstrap.sh
@@ -12,7 +12,7 @@ cp /usr/lib/syslinux/pxelinux.0 /pxe/tftpboot
 cp /usr/lib/syslinux/chain.c32 /pxe/tftpboot
 cp /usr/lib/ipxe/undionly.kpxe /pxe/tftpboot
 
-echo <<EOF >/pxe/tftpboot/map-file
+cat <<EOF >/pxe/tftpboot/map-file
 re ^(/pxe/tftpboot/) /pxe/tftpboot/\2
 re ^/pxe/tftpboot/ /pxe/tftpboot/
 re ^(^/) /pxe/tftpboot/\1
